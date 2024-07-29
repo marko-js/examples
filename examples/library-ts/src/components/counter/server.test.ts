bf3625c3112162fd9e04252105cf42c4070f20a5
dpiercey <dpiercey@ebay.com>
2024-07-29 11:49:38 -0700

    chore: update library-ts setup

--- examples/library-ts/src/components/counter/server.test.ts
+++ examples/library-ts/src/components/counter/server.test.ts
@@ -1,10 +1,11 @@
 import { render, screen } from "@marko/testing-library";
 import { composeStories } from "@storybook/marko";
+
 import * as stories from "./stories";
 
 for (const [name, story] of Object.entries(composeStories(stories))) {
   test(name, async () => {
     await render(story);
-    expect(screen.getByRole("button").outerHTML).toMatchSnapshot();
+    expect(screen.getByRole("button")).toMatchSnapshot();
   });
 }
--- examples/library-ts/src/components/counter/stories.ts
+++ examples/library-ts/src/components/counter/stories.ts
@@ -1,4 +1,5 @@
 import type { Meta, Story } from "@storybook/marko";
+
 import Template, { type Input } from "./index.marko";
 
 export default {
