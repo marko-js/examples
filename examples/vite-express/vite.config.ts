bdc20b519a10c7eb9c9f15925658194b14a5a658
dpiercey <dpiercey@ebay.com>
2026-03-14 20:32:21 -0700

    chore: simplify vite-express example

--- examples/vite-express/vite.config.ts
+++ examples/vite-express/vite.config.ts
@@ -1,15 +1,10 @@
 import type { Request, Response } from "express";
-import { type Connect, defineConfig } from "vite";
+import { defineConfig } from "vite";
 import marko from "@marko/vite";
-
-type App = Promise<typeof import("./src/index.ts")>;
-
-const DEV_ENTRY = "./src/index.ts";
-const PROD_ENTRY = import.meta.resolve("./dist/index.js");
 
 export default defineConfig({
   build: {
-    ssr: DEV_ENTRY,
+    ssr: "./src/index.ts",
     sourcemap: true, // Generate sourcemaps for all builds.
     emptyOutDir: false, // Avoid server & client deleting files from each other.
   },
@@ -18,20 +13,17 @@ export default defineConfig({
     {
       name: "preview",
       configureServer(server) {
-        server.middlewares.use(
-          render(() => server.ssrLoadModule(DEV_ENTRY) as App),
-        );
-      },
-      configurePreviewServer(server) {
-        server.middlewares.use(render(() => import(PROD_ENTRY) as App));
+        server.middlewares.use(async (req, res, next) => {
+          try {
+            const { router } = (await server.ssrLoadModule(
+              "./src/index.ts",
+            )) as typeof import("./src/index.ts");
+            router(req as Request, res as Response, next);
+          } catch (err) {
+            next(err);
+          }
+        });
       },
     },
   ],
 });
-
-function render(app: () => App): Connect.NextHandleFunction {
-  return (req, res, next) =>
-    app()
-      .then(({ router }) => router(req as Request, res as Response, next))
-      .catch(next);
-}
